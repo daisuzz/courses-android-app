8f8c771c3e73820719661e514d5575c0458dfc8d
daisuzz <daisaku.rugby@gmail.com>
2024-08-14 20:28:44 +0900

    first commit

---
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -0,0 +1,126 @@
+package com.example.courses
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.courses.ui.theme.CoursesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CoursesTheme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CoursesApp(Modifier.fillMaxSize())
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CoursesApp(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+        items(DataSource.topics) { topic ->
+            CourseCard(
+                imageResourceId = topic.imageResourceId,
+                stringResourceId = topic.stringResourceId,
+                courseCount = topic.courseCount,
+            )
+        }
+    }
+}
+
+@Composable
+fun CourseCard(
+    imageResourceId: Int,
+    stringResourceId: Int,
+    courseCount: Int,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier.height(68.dp)
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = imageResourceId),
+                contentDescription = stringResource(id = stringResourceId),
+                modifier = Modifier
+                    .width(68.dp)
+                    .fillMaxHeight()
+            )
+            Column(
+                modifier = Modifier.padding(
+                    top = 16.dp,
+                    start = 16.dp,
+                    end = 16.dp
+                )
+            ) {
+                Text(
+                    text = stringResource(stringResourceId),
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_grain),
+                        contentDescription = null,
+                    )
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = courseCount.toString(),
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CoursesAppPreview() {
+    CoursesApp()
+}
